671a0601c82d6caeccdd398b01fa409013277349
Nestor <nestor.venegas95@gmail.com>
2021-03-03 17:26:15 -0600

    commit tree 2387255e404d3bc2fb69f91b0bc2f931e48132ea

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+    def initialize(side1, side2, side3) #three arguments on initialization. 
+      #Each argument is a length of one of the three sides of the triangle.
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+    end
+
+  #Give your Triangles an instance method, kind that returns, as a symbol, its type
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
